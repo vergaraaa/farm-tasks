bde8554dce067e341e4534060d015ab8fd7bc18a
Edgar Vergara <edgarvm13@hotmail.com>
2024-03-21 11:37:57 +0100

    routes

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,23 @@ app = FastAPI()
 @app.get("/")
 def welcome(): 
     return { "message": "welcome to my fastapi api"}
+
+@app.get("/api/tasks")
+async def get_tasks(): 
+    return "all tasks"
+
+@app.post("/api/tasks")
+async def create_task(): 
+    return "create task"
+
+@app.get("/api/tasks/{id}")
+async def get_task(): 
+    return "single tasks"
+
+@app.put("/api/tasks/{id}")
+async def update_task(): 
+    return "updating task"
+
+@app.delete("/api/tasks/{id}")
+async def delete_task(): 
+    return "delete task"
